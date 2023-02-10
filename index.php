db209a599a8a8f3925d9a5e67e886acd4b009a4a
Radityadhrm <124946275+Radityadhrm@users.noreply.github.com>
2023-02-10 08:44:04 +0700

    innitial_commit

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+	if(isset($_GET['pesan'])){
+		if($_GET['pesan'] == "gagal"){
+			echo "Login gagal! username dan password salah!";
+		}else if($_GET['pesan'] == "logout"){
+			echo "Anda telah berhasil logout";
+		}else if($_GET['pesan'] == "belum_login"){
+			echo"
+			<script>
+				alert('Anda Harus Login Dulu untuk mengakses Halaman Ini !!');
+				document.location.href='index.php'
+			</script>";
+		}
+	}
+	?>
+	
+	
+<!DOCTYPE html>
+<html lang="en" >
+	<head>
+		<meta charshet="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!- - Boostrap CSS - ->
+		<link rel="stylesheet" type="text/css" href="asset/css/bootstrap.min.css">
+		<!- - My CSS - ->
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<!- - Boostrap ICON - ->
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+		<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Rubik:400,700'>
+		<link rel="shortcut icon" href="asset/img/logo/logo.png">
+		<title>Aplikasi Catatan Perjalanan</title>
+	</head>
+	
+	<body>
+	<! -- partial:index.partial.html -->
+		<div class="login-form">
+			<form action="cek_login.php"method="POST">
+				<h1><img src="kcp.png"height="50px"><p>Silahkan Login</h1>
+					<div class="content">
+						<div class="input-field">
+							<input type="number"name="nik" placeholder="Masukan NIK"required>
+						</div>
+						<div class="input-field">
+							<input type="text"name="nama" placeholder="Masukan Nama Lengkap"required>
+						</div>
+					</div>
+		<div class="action">
+			<button onclick="window.location.href='register.php'" class="btn mr-2">Saya Pengguna Baru</button>
+			<button type="submit">Masuk</button>
+		</div>
+			</form>
+		</div>
+<! -- partial -- >
+<script src="script.js"></script>
+	</body>
+</html>
